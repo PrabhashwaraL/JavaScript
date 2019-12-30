eec0f49ebc626d228cf51e556b8a6e74ed103997
prabhashwara <lankeshprabhashwara@gmail.com>
2019-12-30 23:56:19 +0530

    Updated callback-function.js

--- callback-function.js
+++ callback-function.js
@@ -1,3 +1,4 @@
+// callback function
 const calcTwoNum = (a, b, addNum) => {
     console.log(`Sum of ${a} and ${b}: ${addNum(a, b)}`);
 };
@@ -5,3 +6,8 @@ const calcTwoNum = (a, b, addNum) => {
 const addNum = (a, b) => a+b;
 
 calcTwoNum(10, 20, addNum);
+
+//Same thing without callback function
+const calcTwoNum2 = (a, b) => console.log(`Sum of ${a} and ${b}: ${addNum(a, b)}`);
+
+calcTwoNum2(10, 20);
